a52a6d9b53264e04eab7ba3ebdaf6640426f59c5
Search-Engine-Optimist <ashleybird33@googlemail.com>
2025-09-14 22:56:22 +0100

    Update vitest.config.mjs

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,10 +1,10 @@
 // vitest.config.mjs
 export default {
   test: {
+    environment: 'node',
+    include: ['tests/**/*.spec.{js,mjs,ts}'],
     coverage: {
-      provider: 'v8',
-      all: false,
-      thresholds: { statements: 0.6, branches: 0.5, functions: 0.6, lines: 0.6 }
+      provider: 'v8'
     }
   }
 }
